6b004f4da11d0098d12e53f1340dcdbd36862e03
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2017-02-05 23:42:17 +0100

    [Vogella introduction 10.7] textfield, button, popup

--- app/src/main/java/com/tomaszbartnik/firstapp/MainActivity.java
+++ app/src/main/java/com/tomaszbartnik/firstapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.tomaszbartnik.firstapp;
 
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick (View view) {
+        EditText input = (EditText) findViewById(R.id.main_input);
+        String string = input.getText().toString();
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+/*        Toast.makeText(this, "Button 1 pressed",
+                Toast.LENGTH_LONG).show();*/
+    }
+
 }
