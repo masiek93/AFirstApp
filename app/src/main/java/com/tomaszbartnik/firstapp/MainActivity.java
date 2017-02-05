21b0ccd71714f98217160b2b37f19a4faac29b15
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2017-02-06 00:01:47 +0100

    [Vogella introduction 11.3] radio buttons added

--- app/src/main/java/com/tomaszbartnik/firstapp/MainActivity.java
+++ app/src/main/java/com/tomaszbartnik/firstapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                }
+            }
+        });
     }
 
     public void onClick (View view) {
